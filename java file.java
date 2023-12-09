a160423c2dfc17ff177700d7a37938fb778d3a15
Sudharsan B <25sudharsan27@gmail.com>
2023-12-09 21:24:37 +0530

    first commit after a long time

---
+++ java file.java
@@ -0,0 +1,11 @@
+const nemo=["nemo"];
+
+function findNemo(array) {
+    for (le i =0; i < array.length; i++) {
+        if (array[i] === "nemo") {
+            console.log("Found NEMO!");
+        }
+    }
+    
+}
+FindNemo(nemo);
---
+++ java.js
@@ -0,0 +1,12 @@
+let a= 5+6
+
+console.log(a)
+
+
+const prompt = require("prompt-sync")();
+
+const input = prompt("What is your name? ");
+
+console.log(`Oh, so your name is ${input}`);
+
+console.log("Hello world");
